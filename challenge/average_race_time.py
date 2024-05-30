24c0ddbb9211c0cd13f741dd47f29d8815e957e5
dkalashnikov <dkalashnikov@gmail.com>
2024-05-30 18:50:55 +0000

    done

--- challenge/average_race_time.py
+++ challenge/average_race_time.py
@@ -2,7 +2,8 @@
 # This dataset has race times for women 10k runners from the Association of Road Racing Statisticians
 
 import re
-import datetime
+from datetime import time
+from math import floor
 
 def get_data():
     """Return content from the 10k_racetimes.txt file"""
@@ -13,7 +14,8 @@ def get_data():
 def get_rhines_times():
     """Return a list of Jennifer Rhines' race times"""
     races = get_data()
-    pass
+    rhines_rows = [re.split(r"\s\s+", race) for race in races.splitlines()]
+    return [row[1] for row in rhines_rows if row[2].find('Jennifer Rhines') == 0]
 
 def get_average():
     """Return Jennifer Rhines' average race time in the format:
@@ -22,4 +24,27 @@ def get_average():
        s corresponds to a seconds digit
        M corresponds to a milliseconds digit (no rounding, just the single digit)"""
     racetimes = get_rhines_times()
-    pass
+    sum = 0.0
+    num = 0
+    for racetime in racetimes:
+        match = re.match(r"(\d+):(\d+)(\.\d+)?", racetime)
+        if match:
+            min = int(match.group(1))
+            sec = int(match.group(2))
+            mil = match.group(3)
+            if mil:
+                frac = float('0' + mil)
+            else:
+                frac = 0.0
+            sum = sum + 60*min + sec + frac
+            num = num + 1
+    avg_sec = sum/num
+    min = avg_sec//60
+    sec = floor(avg_sec) - min*60
+    ms = avg_sec - min*60 - sec
+    ret = "%d:%d" % (min, sec)
+    if ms:
+        ret = ret + ("%1.1f" % ms)[1:]
+    return ret
+    
+
