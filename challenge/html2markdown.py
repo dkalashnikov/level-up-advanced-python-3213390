baa9223c77a61315ad68610f4a6b0f78f66a707a
dkalashnikov <dkalashnikov@gmail.com>
2024-06-04 19:43:07 +0000

    html2markdown

--- challenge/html2markdown.py
+++ challenge/html2markdown.py
@@ -1,4 +1,8 @@
-
+import re
 def html2markdown(html):
     '''Take in html text as input and return markdown'''
-    pass
+    md = re.sub(r"<em>(.+?)</em>", r"*\1*", html)
+    md = re.sub(r"(\s+)", " ", md)
+    md = re.sub(r"<p>(.+?)</p>", r"\1\n\n", md).rstrip()
+    md = re.sub(r"<a href=\"(.+?)\">(.+?)</a>", r"[\2](\1)", md)
+    return md
