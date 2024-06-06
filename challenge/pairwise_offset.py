5bbd142ca74db125c55501f0a85ee2e73d2c46b1
dkalashnikov <dkalashnikov@gmail.com>
2024-06-06 20:13:37 +0000

    pairwise_offset

--- challenge/pairwise_offset.py
+++ challenge/pairwise_offset.py
@@ -1,2 +1,15 @@
-def pairwise_offset(sequence, fillvalue, offset):
-    pass
+def pairwise_offset(sequence, fillvalue='*', offset=0):
+    for i in range(0, len(sequence)+offset):
+        try:
+            first = sequence[i]
+        except IndexError:
+            first = fillvalue
+
+        sec_index = i-offset
+        try:
+            sec = sequence[sec_index]
+            if sec_index < 0:
+                sec = fillvalue
+        except IndexError:
+            sec = fillvalue
+        yield (first, sec)
